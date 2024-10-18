f9c672abc6c0d727b9070bb2854d57cd938cb8c8
Melashenko Ivan <melashenkoivan98@gmail.com>
2024-10-18 20:19:09 +0300

    каким то образом закрыл ветку, открыть не получилось, удалил старую. Создал новую ветку так проще

--- parcel.go
+++ parcel.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 )
 
 type ParcelStore struct {
@@ -14,9 +15,25 @@ func NewParcelStore(db *sql.DB) ParcelStore {
 
 func (s ParcelStore) Add(p Parcel) (int, error) {
 	// реализуйте добавление строки в таблицу parcel, используйте данные из переменной p
+	stmt, err := s.db.Prepare("INSERT INTO parcel (client, status, address, created_at) VALUES (?, ?, ?, ?)")
+	if err != nil {
+		return 0, err
+	}
+	defer stmt.Close()
+
+	// Выполняем запрос с данными из переменной p
+	res, err := stmt.Exec(p.Client, p.Status, p.Address, p.CreatedAt)
+	if err != nil {
+		return 0, err
+	}
 
 	// верните идентификатор последней добавленной записи
-	return 0, nil
+	id, err := res.LastInsertId()
+	if err != nil {
+		return int(id), err
+	}
+
+	return int(id), nil
 }
 
 func (s ParcelStore) Get(number int) (Parcel, error) {
@@ -25,8 +42,22 @@ func (s ParcelStore) Get(number int) (Parcel, error) {
 
 	// заполните объект Parcel данными из таблицы
 	p := Parcel{}
+	query := "SELECT number, client, status, address, created_at FROM parcel WHERE number = ?"
+
+	row := s.db.QueryRow(query, number)
+
+	err := row.Scan(&p.Number, &p.Client, &p.Status, &p.Address, &p.CreatedAt)
+	if err != nil {
+
+		if err == sql.ErrNoRows {
+			return p, fmt.Errorf("посылка с номером %d не найдена", number)
+		}
+
+		return p, err
+	}
 
 	return p, nil
+
 }
 
 func (s ParcelStore) GetByClient(client int) ([]Parcel, error) {
@@ -36,25 +67,94 @@ func (s ParcelStore) GetByClient(client int) ([]Parcel, error) {
 	// заполните срез Parcel данными из таблицы
 	var res []Parcel
 
+	query := "SELECT number, client, status, address, created_at FROM parcel WHERE client = ?"
+
+	rows, err := s.db.Query(query, client)
+	if err != nil {
+
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+
+		var p Parcel
+
+		if err := rows.Scan(&p.Number, &p.Client, &p.Status, &p.Address, &p.CreatedAt); err != nil {
+			return nil, err
+		}
+		res = append(res, p)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
 	return res, nil
 }
 
 func (s ParcelStore) SetStatus(number int, status string) error {
 	// реализуйте обновление статуса в таблице parcel
+	stmt, err := s.db.Prepare("UPDATE parcel SET status = ? WHERE number = ?")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
 
-	return nil
+	_, err = stmt.Exec(status, number)
+	return err
 }
 
 func (s ParcelStore) SetAddress(number int, address string) error {
 	// реализуйте обновление адреса в таблице parcel
-	// менять адрес можно только если значение статуса registered
+	// менять адрес можно только если значение статуса registered"
+	stmt, err := s.db.Prepare("UPDATE parcel SET address = ? WHERE number = ? AND status = ?")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	result, err := stmt.Exec(address, number, ParcelStatusRegistered)
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rowsAffected == 0 {
+		return fmt.Errorf("нельзя изменить адрес, так как статус посылки не 'registered'")
+	}
 
 	return nil
+
 }
 
 func (s ParcelStore) Delete(number int) error {
 	// реализуйте удаление строки из таблицы parcel
 	// удалять строку можно только если значение статуса registered
+	stmt, err := s.db.Prepare("DELETE FROM parcel WHERE number = ? AND status = ?")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	result, err := stmt.Exec(number, ParcelStatusRegistered)
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rowsAffected == 0 {
+		return fmt.Errorf("нельзя удалить посылку, так как статус не 'registered'")
+	}
 
 	return nil
+
 }
